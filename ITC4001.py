bf18110573037e324ee048ba624e4f46cf901f7a
yuany3721 <li1116@mail.ustc.edu.cn>
2022-11-28 19:47:16 +0800

    decoupling

--- ITC4001.py
+++ ITC4001.py
@@ -1,36 +1,8 @@
+from VISAInstrument import VISAInstrument
 from datetime import datetime
-import pyvisa as visa
 
 
-class ITC4001:
-    manufacturer = 'USTC'
-    model = 'VISAInstrument'
-
-    def __init__(self, resourceID: str, timeout: int, reset: bool):
-        self.resourceID = resourceID
-        # connect
-        try:
-            self.resource = visa.ResourceManager().open_resource(resourceID)
-            self.resource.timeout = timeout
-        except BaseException as e:
-            print('Error in open device at: {}'.format(resourceID), e)
-        # instrument infomation
-        print(datetime.now(), self.query("*IDN?").strip().replace(",", " "), "connected.")
-        # reset
-        if reset:
-            self.resource.write("*RST")
-
-    def write(self, command: str):
-        return self.resource.write(command)
-
-    def query(self, command: str):
-        return self.resource.query(command)
-
-    def command(self, command: str):
-        if command.endswith("?"):
-            return self.query(command)
-        else:
-            return self.write(command)
+class ITC4001(VISAInstrument):
 
     def print_info(self):
         # print instrument info
